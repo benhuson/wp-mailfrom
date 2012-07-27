0736b47e438c81f3d916073ef09925b8cfe4f836
Frederik Svarre <FRS@FSLab.dk>
2012-07-27 20:02:11 +0200

    Plugin made ready for translation + Danish translation added.

--- wp-mailfrom.php
+++ wp-mailfrom.php
@@ -12,23 +12,24 @@ Released under the GPL license
 http://www.gnu.org/licenses/gpl.txt
 */
 
+load_plugin_textdomain( 'wp-mailfrom', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
 function site_mail_from_option_page() {
   ?>
   <div class="wrap">
-  <h2>mailfrom options</h2>
-  <i>If set, these 2 options will override the email address and name in the <strong>From:</strong> header on all sent emails</i>
+  <h2><?php _e('mailfrom options', 'wp-mailfrom'); ?></h2>
+  <i><?php _e('If set, these 2 options will override the email address and name in the <strong>From:</strong> header on all sent emails', 'wp-mailfrom'); ?></i>
   <form method="post" action="options.php">
   <?php wp_nonce_field('update-options'); ?>
   <table class="form-table">
-  <tr><th scope="row">From email address</th>
+  <tr><th scope="row"><? _e('From email address', 'wp-mailfrom'); ?></th>
   <td><input type="text" name="site_mail_from_email" value="<?php echo get_option('site_mail_from_email'); ?>" /></td></tr>
-  <tr><th scope="row">From email name</th>
+  <tr><th scope="row"><? _e('From email name', 'wp-mailfrom'); ?></th>
   <td><input type="text" name="site_mail_from_name" value="<?php echo get_option('site_mail_from_name'); ?>" /></td></tr>
   </table>
   <input type="hidden" name="action" value="update" />
   <input type="hidden" name="page_options" value="site_mail_from_email,site_mail_from_name" />
   <p class="submit">
-  <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
+  <input type="submit" name="Submit" value="<?php _e('Save Changes', 'wp-mailfrom'); ?>" />
   </p>
   </form>
   </div>
