b2ba0a8373d5f5c641f1c7f3dba215942bfd7dd0
Ben Huson <ben@thewhiteroom.net>
2012-07-27 17:53:23 +0100

    Version 1.0

--- readme.txt
+++ readme.txt
@@ -1,41 +1,72 @@
-=== wp mail from ===
-Contributors: Tristan Aston
+=== WP Mail From ===
+Contributors: husobj, Tristan Aston
 Tags: mail from, from email, email from, from address, mail, email, smtp, from address, email address, from header
-Requires at least: 2.3
-Tested up to: 3.1
-Stable tag: 0.6
+Requires at least: 2.9
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Allows you to configure the default email address and name used on email sent by wordpress.
+Allows you to configure the default email address and name used for emails sent by WordPress.
 
 == Description ==
 
-This plugin allows you to set the email address and name used on email sent by wordpress by setting the *From:* header.
+This plugin allows you to set the email address and name used on email sent by WordPress by setting the *From:* header.
 
-* Adds a "wp mail from" section in the "settings" menu
-* The plugin uses the filter hooks **wp\_mail\_from** and **wp\_mail\_from\_name**. I believe these are both available in WordPress 2.3+
+* Adds a "Mail From" section in the "Settings" menu.
+* The plugin uses the filter hooks `wp_mail_from` and `wp_mail_from_name`.
 * The priority for the hooks is set to 1 to allow for other plugins that may hook these with the default priority of 10 to override this plugin.
 
 == Installation ==
 
-1. Unzip `wp-mailfrom.zip` in the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
+Either install via the WordPress admin plugin installer or...
+
+1. Unzip `wp-mailfrom.zip` in the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
 
 == Frequently Asked Questions ==
 
-= Why does the From address still show as the default or show up as 'sent on behalf of' the default address =
+= Why does the From address still show as the default or show up as 'sent on behalf of' the default address? =
 
 Possibly your mail server has added a *Sender:* header or is configured to always set the *envelope sender* to the user calling it.
 
 == Screenshots ==
 
-1. The settings menu
+1. The settings page
 
 == Changelog ==
 
+= 1.0 =
+* Pretty much re-coded from scratch - now based around a core WP_MailFrom class.
+* Uses the [WordPress Settings API](http://codex.wordpress.org/Settings_API).
+* Stores name and email as `wp_mailfrom_name` and `wp_mailfrom_email` options. Legacy support provided for old options.
+
 = 0.6 =
-readme.txt change to reflect compatibility with recent versions of wordpress.
+* readme.txt changed to reflect compatibility with recent versions of WordPress.
 
 == Upgrade Notice ==
 
-= 0.6 =
-This version just updates compatibility information
+= 1.0 =
+This version is pretty much a complete re-write, fixes loads of bugs and works with the most recent versions of WordPress.
+
+= 0.6 = 
+This version just updates compatibility information.
+
+== Theme/Plugin Authors ==
+
+= 1.0 =
+This version is pretty much a complete re-write.
+If you have been using a previous version of this plugin it should just work.
+
+However, for naming consistency, the option names have changed.
+If you accessed either of the options directly for any reason you will need to re-factor you code as support for the old options will be removed in a future version.
+
+The old options could be retrieved as follows:
+
+`get_option( 'site_mail_from_name' );
+get_option( 'site_mail_from_email' );`
+
+You should now use:
+
+`get_option( 'wp_mailfrom_name' );
+get_option( 'wp_mailfrom_email' );`
--- wp-mailfrom.php
+++ wp-mailfrom.php
@@ -1,64 +1,188 @@
 <?php 
 
 /*
-Plugin Name: wp mail from
-Version: 0.6
+Plugin Name: WP Mail From
+Version: 1.0
 Plugin URI: http://wordpress.org/extend/plugins/wp-mailfrom/
-Description: This plugin allows you to set the From header on emails sent by wordpress
-Author: Tristan Aston
+Description: Allows you to configure the default email address and name used for emails sent by WordPress.
+Author: Tristan Aston, updated by Ben Huson
 
-Copyright (c) 2009
+Copyright (c) 2009-2012, 
 Released under the GPL license
 http://www.gnu.org/licenses/gpl.txt
 */
 
-function site_mail_from_option_page() {
-  ?>
-  <div class="wrap">
-  <h2>mailfrom options</h2>
-  <i>If set, these 2 options will override the email address and name in the <strong>From:</strong> header on all sent emails</i>
-  <form method="post" action="options.php">
-  <?php wp_nonce_field('update-options'); ?>
-  <table class="form-table">
-  <tr><th scope="row">From email address</th>
-  <td><input type="text" name="site_mail_from_email" value="<?php echo get_option('site_mail_from_email'); ?>" /></td></tr>
-  <tr><th scope="row">From email name</th>
-  <td><input type="text" name="site_mail_from_name" value="<?php echo get_option('site_mail_from_name'); ?>" /></td></tr>
-  </table>
-  <input type="hidden" name="action" value="update" />
-  <input type="hidden" name="page_options" value="site_mail_from_email,site_mail_from_name" />
-  <p class="submit">
-  <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
-  </p>
-  </form>
-  </div>
-  <?php
+class WP_MailFrom {
+
+	/**
+	 * Constructor
+	 */
+	function WP_MailFrom() {
+		add_action( 'admin_init', array( $this, 'wp_mailfrom_settings' ) );
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		
+		// Name and email filter
+		add_filter( 'wp_mail_from_name', array( $this, 'wp_mail_from_name' ), 1 );
+		add_filter( 'wp_mail_from', array( $this, 'wp_mail_from' ), 1 );
+		
+		// Legacy support for old options - just in case someone used this directly!
+		add_filter( 'pre_option_site_mail_from_name', 'get_option_site_mail_from_name', 1 );
+		add_filter( 'pre_option_site_mail_from_email', 'get_option_site_mail_from_email', 1 );
+	}
+	
+	/**
+	 * Filter: wp_mail_from_name
+	 *
+	 * @param string $name Default name.
+	 * @return string $name WP Mail From name.
+	 */
+	function wp_mail_from_name( $name ) {
+		$wp_mailfrom_name = get_option( 'wp_mailfrom_name', '' );
+		if ( ! empty( $wp_mailfrom_name ) ) {
+			return $wp_mailfrom_name;
+		}
+		return $name;
+	}
+	
+	/**
+	 * Filter: wp_mail_from
+	 *
+	 * @param string $name Default email.
+	 * @return string $name WP Mail From email.
+	 */
+	function wp_mail_from( $email ) {
+		$wp_mailfrom_email = get_option( 'wp_mailfrom_email', '' );
+		if ( ! empty( $wp_mailfrom_email ) ) {
+			return $wp_mailfrom_email;
+		}
+		return $email;
+	}
+	
+	/**
+	 * Admin Menu
+	 */
+	function admin_menu() {
+		add_options_page( __( 'WP Mail From Plugin', 'wp_mailfrom' ), __( 'Mail From', 'wp_mailfrom' ), 'manage_options', 'wp_mailfrom', array( $this, 'settings_page' ) );
+	}
+	
+	/**
+	 * Settings Page
+	 */
+	function settings_page() {
+		?>
+		<div class="wrap">
+			<div id="icon-options-general" class="icon32"><br></div>
+			<h2><?php _e( 'Mail From Settings', 'wp_mailfrom' ); ?></h2>
+			<form action="options.php" method="post">
+				<?php
+				settings_fields( 'wp_mailfrom' );
+				do_settings_sections( 'wp_mailfrom' );
+				?>
+				<p class="submit"><input name="submit" type="submit" value="<?php esc_attr_e( 'Save Changes' ); ?>" /></p>
+			</form>
+		</div>
+		<?php
+	}
+	
+	/**
+	 * Settings API
+	 */
+	function wp_mailfrom_settings() {
+		add_settings_section(
+			'wp_mailfrom',
+			'',
+			array( $this, 'wp_mailfrom_settings_section' ),
+			'wp_mailfrom'
+		);
+		add_settings_field(
+			'wp_mailfrom_name',
+			__( 'From Name', 'wp_mailfrom' ),
+			array( $this, 'wp_mailfrom_name_field' ),
+			'wp_mailfrom',
+			'wp_mailfrom'
+		);
+		add_settings_field(
+			'wp_mailfrom_email',
+			__( 'From Email Address', 'wp_mailfrom' ),
+			array( $this, 'wp_mailfrom_email_field' ),
+			'wp_mailfrom',
+			'wp_mailfrom'
+		);
+ 		register_setting( 'wp_mailfrom', 'wp_mailfrom_name', array( $this, 'sanitize_wp_mailfrom_name' ) );
+ 		register_setting( 'wp_mailfrom', 'wp_mailfrom_email', 'is_email' );
+	}
+
+	/**
+	 * Sanitize Mail From Name
+	 * Strips out all HTML, scripts...
+	 *
+	 * @param string $val Name.
+	 * @return string Sanitized name.
+	 */
+	function sanitize_wp_mailfrom_name( $val ) {
+		return wp_kses( $val, array() );
+	}
+
+	/**
+	 * Mail From Settings Section
+	 */
+	function wp_mailfrom_settings_section() {
+		echo '<p>' . __( 'If set, these 2 options will override the name and email address in the <strong>From:</strong> header on all sent emails.', 'wp_mailfrom' ) . '</p>';
+	}
+
+	/**
+	 * Mail From Name Field
+	 */
+	function wp_mailfrom_name_field() {
+		echo '<input name="wp_mailfrom_name" type="text" id="wp_mailfrom_name" value="' . get_option( 'wp_mailfrom_name', '' ) . '" class="regular-text" />';
+	}
+
+	/**
+	 * Mail From Email Field
+	 */
+	function wp_mailfrom_email_field() {
+		echo '<input name="wp_mailfrom_email" type="text" id="wp_mailfrom_email" value="' . get_option( 'wp_mailfrom_email', '' ) . '" class="regular-text" />';
+	}
+	
+	/**
+	 * Legacy support for get_option( 'site_mail_from_name' )
+	 */
+	function get_option_site_mail_from_name( $option, $default = false ) {
+		return get_option( 'wp_mailfrom_name', $default );
+	}
+	
+	/**
+	 * Legacy support for get_option( 'site_mail_from_email' )
+	 */
+	function get_option_site_mail_from_email( $option, $default = false ) {
+		return get_option( 'wp_mailfrom_email', $default );
+	}
+	
+	/**
+	 * Register Activation
+	 * Perform upgrades etc.
+	 */
+	function register_activation() {
+		
+		// Temporarily remove our filter which provide support for legacy options
+		remove_filter( 'pre_option_site_mail_from_name', 'get_option_site_mail_from_name', 1 );
+		remove_filter( 'pre_option_site_mail_from_email', 'get_option_site_mail_from_email', 1 );
+		
+		// Get old option value and try to assign them to new options
+		$name = get_option( 'site_mail_from_name', '' );
+		$email = get_option( 'site_mail_from_email', '' );
+		$name_updated = add_option( 'wp_mailfrom_name', $name );
+		$email_updated = add_option( 'wp_mailfrom_email', $email );
+		
+		// If new options created delete old options
+		if ( $name_updated )
+			delete_option( 'site_mail_from_name' );
+		if ( $email_updated )
+			delete_option( 'site_mail_from_email' );
+	}
+	
 }
 
-function site_mail_from_menu () {
-  add_options_page('mailfrom plugin', 'wp mail from', 9, __FILE__, 'site_mail_from_option_page');
-}
-
-function site_mail_from ($mail_from_email) {
-  $site_mail_from_email = get_option('site_mail_from_email');
-  if(empty($site_mail_from_email)) {
-    return $mail_from_email;
-  }
-  else {
-    return $site_mail_from_email;
-  }
-}
-
-function site_mail_from_name ($mail_from_name) {
-  $site_mail_from_name = get_option('site_mail_from_name');
-  if(empty($site_mail_from_name)) {
-    return $mail_from_name;
-  }
-  else {
-    return $site_mail_from_name;
-  }
-}
-
-add_action('admin_menu', 'site_mail_from_menu');
-add_filter('wp_mail_from','site_mail_from',1);
-add_filter('wp_mail_from_name','site_mail_from_name',1);
+global $WP_MailFrom;
+$WP_MailFrom = new WP_MailFrom();
+register_activation_hook( __FILE__, array( $WP_MailFrom, 'register_activation' ) );
