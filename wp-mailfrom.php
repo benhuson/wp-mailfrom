57e33409ab651e9d7246b2f21310decd4f9247e9
Ben Huson <ben@thewhiteroom.net>
2012-08-22 14:29:52 +0100

    Re-namespace everything for WP Mail Form II.

--- wp-mailfrom.php
+++ wp-mailfrom.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WP Mail From II
 Version: 1.0
-Plugin URI: http://wordpress.org/extend/plugins/wp-mailfrom/
+Plugin URI: http://wordpress.org/extend/plugins/wp-mailfrom-ii/
 Description: Allows you to configure the default email address and name used for emails sent by WordPress.
 Author: Ben Huson
 
@@ -15,13 +15,13 @@ Released under the GPL license
 http://www.gnu.org/licenses/gpl.txt
 */
 
-class WP_MailFrom {
+class WP_MailFrom_II {
 
 	/**
 	 * Constructor
 	 */
-	function WP_MailFrom() {
-		add_action( 'admin_init', array( $this, 'wp_mailfrom_settings' ) );
+	function WP_MailFrom_II() {
+		add_action( 'admin_init', array( $this, 'settings' ) );
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
 		
@@ -30,15 +30,16 @@ class WP_MailFrom {
 		add_filter( 'wp_mail_from', array( $this, 'wp_mail_from' ), 1 );
 		
 		// Legacy support for old options - just in case someone used this directly!
-		add_filter( 'pre_option_site_mail_from_name', 'get_option_site_mail_from_name', 1 );
-		add_filter( 'pre_option_site_mail_from_email', 'get_option_site_mail_from_email', 1 );
+		// (not really needed unless we can takeover the old plugin)
+		//add_filter( 'pre_option_site_mail_from_name', 'get_option_site_mail_from_name', 1 );
+		//add_filter( 'pre_option_site_mail_from_email', 'get_option_site_mail_from_email', 1 );
 	}
 	
 	/**
 	 * Load Text Domain Language Support
 	 */
 	function load_textdomain() {
-		load_plugin_textdomain( 'wp-mailfrom', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+		load_plugin_textdomain( 'wp-mailfrom-ii', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
 	}
 	
 	/**
@@ -48,9 +49,9 @@ class WP_MailFrom {
 	 * @return string $name WP Mail From name.
 	 */
 	function wp_mail_from_name( $name ) {
-		$wp_mailfrom_name = get_option( 'wp_mailfrom_name', '' );
-		if ( ! empty( $wp_mailfrom_name ) ) {
-			return $wp_mailfrom_name;
+		$wp_mailfrom_ii_name = get_option( 'wp_mailfrom_ii_name', '' );
+		if ( ! empty( $wp_mailfrom_ii_name ) ) {
+			return $wp_mailfrom_ii_name;
 		}
 		return $name;
 	}
@@ -62,9 +63,9 @@ class WP_MailFrom {
 	 * @return string $name WP Mail From email.
 	 */
 	function wp_mail_from( $email ) {
-		$wp_mailfrom_email = get_option( 'wp_mailfrom_email', '' );
-		if ( ! empty( $wp_mailfrom_email ) ) {
-			return $wp_mailfrom_email;
+		$wp_mailfrom_ii_email = get_option( 'wp_mailfrom_ii_email', '' );
+		if ( ! empty( $wp_mailfrom_ii_email ) ) {
+			return $wp_mailfrom_ii_email;
 		}
 		return $email;
 	}
@@ -73,7 +74,7 @@ class WP_MailFrom {
 	 * Admin Menu
 	 */
 	function admin_menu() {
-		add_options_page( __( 'WP Mail From Plugin', 'wp-mailfrom' ), __( 'Mail From', 'wp-mailfrom' ), 'manage_options', 'wp_mailfrom', array( $this, 'settings_page' ) );
+		add_options_page( __( 'WP Mail From Plugin', 'wp-mailfrom-ii' ), __( 'Mail From', 'wp-mailfrom-ii' ), 'manage_options', 'wp_mailfrom', array( $this, 'settings_page' ) );
 	}
 	
 	/**
@@ -83,13 +84,13 @@ class WP_MailFrom {
 		?>
 		<div class="wrap">
 			<div id="icon-options-general" class="icon32"><br></div>
-			<h2><?php _e( 'Mail From Settings', 'wp-mailfrom' ); ?></h2>
+			<h2><?php _e( 'Mail From Settings', 'wp-mailfrom-ii' ); ?></h2>
 			<form action="options.php" method="post">
 				<?php
-				settings_fields( 'wp_mailfrom' );
-				do_settings_sections( 'wp_mailfrom' );
+				settings_fields( 'wp_mailfrom_ii' );
+				do_settings_sections( 'wp_mailfrom_ii' );
 				?>
-				<p class="submit"><input name="submit" type="submit" value="<?php esc_attr_e( 'Save Changes', 'wp-mailfrom' ); ?>" /></p>
+				<p class="submit"><input name="submit" type="submit" value="<?php esc_attr_e( 'Save Changes', 'wp-mailfrom-ii' ); ?>" /></p>
 			</form>
 		</div>
 		<?php
@@ -98,29 +99,29 @@ class WP_MailFrom {
 	/**
 	 * Settings API
 	 */
-	function wp_mailfrom_settings() {
+	function settings() {
 		add_settings_section(
-			'wp_mailfrom',
+			'wp_mailfrom_ii',
 			'',
-			array( $this, 'wp_mailfrom_settings_section' ),
-			'wp_mailfrom'
+			array( $this, 'settings_section' ),
+			'wp_mailfrom_ii'
 		);
 		add_settings_field(
-			'wp_mailfrom_name',
-			__( 'From Name', 'wp-mailfrom' ),
-			array( $this, 'wp_mailfrom_name_field' ),
-			'wp_mailfrom',
-			'wp_mailfrom'
+			'wp_mailfrom_ii_name',
+			__( 'From Name', 'wp-mailfrom-ii' ),
+			array( $this, 'wp_mailfrom_ii_name_field' ),
+			'wp_mailfrom_ii',
+			'wp_mailfrom_ii'
 		);
 		add_settings_field(
-			'wp_mailfrom_email',
-			__( 'From Email Address', 'wp-mailfrom' ),
-			array( $this, 'wp_mailfrom_email_field' ),
-			'wp_mailfrom',
-			'wp_mailfrom'
+			'wp_mailfrom_ii_email',
+			__( 'From Email Address', 'wp-mailfrom-ii' ),
+			array( $this, 'wp_mailfrom_ii_email_field' ),
+			'wp_mailfrom_ii',
+			'wp_mailfrom_ii'
 		);
- 		register_setting( 'wp_mailfrom', 'wp_mailfrom_name', array( $this, 'sanitize_wp_mailfrom_name' ) );
- 		register_setting( 'wp_mailfrom', 'wp_mailfrom_email', 'is_email' );
+ 		register_setting( 'wp_mailfrom_ii', 'wp_mailfrom_ii_name', array( $this, 'sanitize_wp_mailfrom_ii_name' ) );
+ 		register_setting( 'wp_mailfrom_ii', 'wp_mailfrom_ii_email', 'is_email' );
 	}
 
 	/**
@@ -130,43 +131,43 @@ class WP_MailFrom {
 	 * @param string $val Name.
 	 * @return string Sanitized name.
 	 */
-	function sanitize_wp_mailfrom_name( $val ) {
+	function sanitize_wp_mailfrom_ii_name( $val ) {
 		return wp_kses( $val, array() );
 	}
 
 	/**
 	 * Mail From Settings Section
 	 */
-	function wp_mailfrom_settings_section() {
-		echo '<p>' . __( 'If set, these 2 options will override the name and email address in the <strong>From:</strong> header on all sent emails.', 'wp-mailfrom' ) . '</p>';
+	function settings_section() {
+		echo '<p>' . __( 'If set, these 2 options will override the name and email address in the <strong>From:</strong> header on all sent emails.', 'wp-mailfrom-ii' ) . '</p>';
 	}
 
 	/**
 	 * Mail From Name Field
 	 */
-	function wp_mailfrom_name_field() {
-		echo '<input name="wp_mailfrom_name" type="text" id="wp_mailfrom_name" value="' . get_option( 'wp_mailfrom_name', '' ) . '" class="regular-text" />';
+	function wp_mailfrom_ii_name_field() {
+		echo '<input name="wp_mailfrom_ii_name" type="text" id="wp_mailfrom_ii_name" value="' . get_option( 'wp_mailfrom_ii_name', '' ) . '" class="regular-text" />';
 	}
 
 	/**
 	 * Mail From Email Field
 	 */
-	function wp_mailfrom_email_field() {
-		echo '<input name="wp_mailfrom_email" type="text" id="wp_mailfrom_email" value="' . get_option( 'wp_mailfrom_email', '' ) . '" class="regular-text" />';
+	function wp_mailfrom_ii_email_field() {
+		echo '<input name="wp_mailfrom_ii_email" type="text" id="wp_mailfrom_ii_email" value="' . get_option( 'wp_mailfrom_ii_email', '' ) . '" class="regular-text" />';
 	}
 	
 	/**
 	 * Legacy support for get_option( 'site_mail_from_name' )
 	 */
 	function get_option_site_mail_from_name( $option, $default = false ) {
-		return get_option( 'wp_mailfrom_name', $default );
+		return get_option( 'wp_mailfrom_ii_name', $default );
 	}
 	
 	/**
 	 * Legacy support for get_option( 'site_mail_from_email' )
 	 */
 	function get_option_site_mail_from_email( $option, $default = false ) {
-		return get_option( 'wp_mailfrom_email', $default );
+		return get_option( 'wp_mailfrom_ii_email', $default );
 	}
 	
 	/**
@@ -176,24 +177,32 @@ class WP_MailFrom {
 	function register_activation() {
 		
 		// Temporarily remove our filter which provide support for legacy options
+		// (is only really needed if we can takeover the old plugin, but leave in for now)
 		remove_filter( 'pre_option_site_mail_from_name', 'get_option_site_mail_from_name', 1 );
 		remove_filter( 'pre_option_site_mail_from_email', 'get_option_site_mail_from_email', 1 );
 		
 		// Get old option value and try to assign them to new options
 		$name = get_option( 'site_mail_from_name', '' );
 		$email = get_option( 'site_mail_from_email', '' );
-		$name_updated = add_option( 'wp_mailfrom_name', $name );
-		$email_updated = add_option( 'wp_mailfrom_email', $email );
+		$new_name = get_option( 'wp_mailfrom_ii_name', '' );
+		$new_email = get_option( 'wp_mailfrom_ii_email', '' );
+		if ( ! empty( $name ) && empty( $new_name ) )
+			$name_updated = add_option( 'wp_mailfrom_ii_name', $name );
+		if ( ! empty( $email ) && empty( $new_email ) )
+			$email_updated = add_option( 'wp_mailfrom_ii_email', $email );
 		
 		// If new options created delete old options
+		// (don't do this at the moment, only if we can takeover the old plugin)
+		/*
 		if ( $name_updated )
 			delete_option( 'site_mail_from_name' );
 		if ( $email_updated )
 			delete_option( 'site_mail_from_email' );
+		*/
 	}
 	
 }
 
-global $WP_MailFrom;
-$WP_MailFrom = new WP_MailFrom();
-register_activation_hook( __FILE__, array( $WP_MailFrom, 'register_activation' ) );
+global $WP_MailFrom_II;
+$WP_MailFrom_II = new WP_MailFrom_II();
+register_activation_hook( __FILE__, array( $WP_MailFrom_II, 'register_activation' ) );
