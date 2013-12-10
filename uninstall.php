dc1d0a961c5caf1eb7ebda75d43babd3ffe49dd2
Ben Huson <ben@thewhiteroom.net>
2013-12-10 19:52:50 +0000

    Delete plugin options when uninstalled.

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Delete options
+delete_option( 'wp_mailfrom_ii_name' );
+delete_option( 'wp_mailfrom_ii_email' );
